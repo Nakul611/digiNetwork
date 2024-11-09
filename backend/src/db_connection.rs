002c03d49dbef92c8e52dd998ab4aaf860c67494
Nakul Balaiwar <93568648+Nakul611@users.noreply.github.com>
2024-11-09 23:56:31 +0530

    Delete backend/src/db_connection.rs

--- backend/src/db_connection.rs
+++
@@ -1,8 +0,0 @@
-use postgres::{Client, Error};
-
-pub fn run() -> Result<(), Error> {
-    // Connect to the PostgreSQL database
-    let mut client = Client::connect("postgresql://postgres:qwertyasdfgh@localhost/", postgres::NoTls)?; 
-      
-    Ok(())
-}
